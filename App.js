aa954aa1bcfb09e1a6ce8fa657c4a12645c4e6b9
siya <siya@uber5.com>
2020-04-16 16:45:55 +0200

    added assests i.e. fonts. Included the splash package to allow control over splash screen. Doing the setup for the splashscreen for both IOS and Android

--- App.js
+++ App.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {View, Text} from 'react-native';
+import SplashScreen from 'react-native-splash-screen'
 
 const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  });
+
   return (
     <View>
       <Text>d</Text>
